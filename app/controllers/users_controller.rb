b58dc8bfc6c61e24d79e6bb6f2541617f794ac8a
ab170301 <antonia.blazevic173@gmail.com>
2022-03-07 11:20:14 +0100

    Add my friends view and search friends form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,29 @@ class UsersController < ApplicationController
     @tracked_stocks = current_user.stocks # devise helper
   end
 
+  def my_friends
+    @friends = current_user.friends
+  end
+
+  def search
+    if params[:friend].present?
+      @friend = params[:friend] 
+      if @friend
+        respond_to do |format|
+          format.js { render partial: 'users/friends_result' }
+        end
+      else
+        respond_to do |format|
+          flash.now[:alert] = "Couldn't find user"
+          format.js { render partial: 'users/friends_result' }
+        end
+      end
+    else 
+      respond_to do |format|
+        flash.now[:alert] = "Please enter a friend name or email to search"
+        format.js { render partial: 'users/friends_result' }
+      end
+    end
+  end
+
 end
